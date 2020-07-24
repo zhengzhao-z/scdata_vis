a0722e76b7e9658f18dcd45635f3f1cf7494bb61
zhengzhao-z <1058021038@qq.com>
2020-07-24 14:21:43 +0800

    init

--- client/src/main.js
+++ client/src/main.js
@@ -3,8 +3,21 @@
 import Vue from 'vue'
 import App from './App'
 
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import axios from 'axios'
+import * as d3 from "d3";
+
+
+Vue.use(ElementUI);
+Vue.prototype.$axios = axios;
+Vue.prototype.$d3 = d3;
+window.d3 = d3; //暂时设置为全局变量
+
+
 Vue.config.productionTip = false
 
+Vue.pro
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
---
+++ server/server.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: zhengzhao
+ * @Date: 2020-07-24 14:20:26
+ * @LastEditTime: 2020-07-24 14:20:41
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \scdata_vis\server\server.js
+ */ 
