812cad73f923a8029c6631b4927f0788731583b3
tsai <mengjietsai@163.com>
2020-07-26 18:09:24 +0800

    T地图初始化 热力图初始化 地图轮廓初始化

--- client/src/App.vue
+++ client/src/App.vue
@@ -5,25 +5,23 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-import tmap from './components/map'
+import HelloWorld from "./components/HelloWorld";
+import tmap from "./components/map";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     HelloWorld,
     tmap
   }
-}
+};
 </script>
 
 <style>
+html,
+body,
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
+  width: 100%;
 }
 </style>
