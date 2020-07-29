00cad3bbecfcbbb6b56e630793798e28a9b9a223
zhengzhao-z <1058021038@qq.com>
2020-07-29 20:40:35 +0800

    解决了一些冲突

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-
-=======
-<!--
- * @Description: 
- * @version: 0.1
- * @Author: zhengzhao
- * @LastEditor: zhengzhao
--->
->>>>>>> 14cd39672f78f4ed77246360fbd9ae2e50614b41
 <!--
  * @Description: 
  * @version: 0.1
@@ -16,35 +6,9 @@
 -->
 <template>
   <div id="app">
-<<<<<<< HEAD
     <traffic></traffic>
     <tmap></tmap>
     <control></control>
-=======
-    <!-- <traffic></traffic> -->
-    <!-- <tmap></tmap> -->
-    <!-- <OV></OV> -->
-    <input
-      type="checkbox"
-      id="公路施工养护"
-      value="公路施工养护"
-      v-model="selectedLiebie"
-    />
-    <label for="公路施工养护">公路施工养护</label>
-    <input
-      type="checkbox"
-      id="车辆交通事故"
-      value="车辆交通事故"
-      v-model="selectedLiebie"
-    />
-    <label for="车辆交通事故">车辆交通事故</label>
-    <input type="checkbox" id="其他" value="其他" v-model="selectedLiebie" />
-    <label for="其他">其他</label>
-    <br />
-    <span>Checked names: {{ selectedLiebie }}</span>
-    <div><button @click="sendMessage">提交</button></div>
-    <calendar></calendar>
->>>>>>> 14cd39672f78f4ed77246360fbd9ae2e50614b41
   </div>
 </template>
 
@@ -53,12 +17,9 @@ import HelloWorld from "./components/HelloWorld";
 import tmap from "./components/map";
 import OV from "./components/overView";
 import traffic from "./components/traffice";
-<<<<<<< HEAD
 import control from "./components/control"
-
-=======
 import calendar from "./components/calendar";
->>>>>>> 14cd39672f78f4ed77246360fbd9ae2e50614b41
+
 export default {
   name: "App",
   data() {
@@ -73,11 +34,8 @@ export default {
     tmap,
     traffic,
     OV,
-<<<<<<< HEAD
-    control
-=======
+    control,
     calendar
->>>>>>> 14cd39672f78f4ed77246360fbd9ae2e50614b41
   }
 };
 </script>
